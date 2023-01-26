cf709bdd9f2759f4d1c69e4003d5e01aaf0f2b4c
mandy94 <majasavic199@gmail.com>
2023-01-26 15:41:49 +0100

    App configs

--- src/app/appointments.service.ts
+++ src/app/appointments.service.ts
@@ -5,6 +5,7 @@ const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
+    
     'Authorization': 'Bearer ' + localStorage.getItem('access-token')
   })
 };
